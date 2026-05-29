fdbc55c4403e213f8b9425c18b8bed5ba43f14cf
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-05-29 23:19:13 +0530

    add

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://study-collab-9pcp.onrender.comapi',
+  baseURL: 'api',
   headers: {
     'Content-Type': 'application/json',
   },
